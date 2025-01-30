e0df05e468e4de1d797c01437301a551744a4e63
Achmad F. Ibrahim <acfatah@gmail.com>
2025-01-30 15:00:54 +0800

    feat: add logestic logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Elysia } from 'elysia'
 import { helmet } from 'elysia-helmet'
+import { Logestic } from 'logestic'
 import { router } from './routes'
 
 const PORT = process.env.PORT || 3000
 
 const app = new Elysia()
+  .use(Logestic.preset('common'))
   .use(helmet())
   .use(router)
   .listen(PORT)
